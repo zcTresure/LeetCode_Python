0e7bf1e8a0319d43b8cc4fa4a235b0dd64a3ba04
zc9442664 <1260638417@qq.com>
2020-09-06 17:56:52 +0800

    dell_2

---
+++ question_bank/剑指 Offer 48. 最长不含重复字符的子字符串.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        dic, res, i = {}, 0, -1
+        for j in range(len(s)):
+            if s[j] in dic:
+                i = max(dic[s[j]], i)
+            dic[s[j]] = j
+            res = max(res, j - i)
+        return res
+
+
+s = "abcabcbb"
+test = Solution()
+print(test.lengthOfLongestSubstring(s))
