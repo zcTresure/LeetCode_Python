635dbbf1752eb084c4c4168290a8fdc2efc49d71
zc9442664 <1260638417@qq.com>
2020-09-11 19:47:10 +0800

    lab

---
+++ question_bank/884. 两句话中的不常见单词.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uncommonFromSentences(self, A: str, B: str) -> list:
+        count = {}
+        for word in A.split():
+            count[word] = count.get(word, 0) + 1
+        for word in B.split():
+            count[word] = count.get(word, 0) + 1
+        return [word for word in count if count[word] == 1]
+
+
+A = "apple apple"
+B = "banana"
+print(Solution().uncommonFromSentences(A, B))
