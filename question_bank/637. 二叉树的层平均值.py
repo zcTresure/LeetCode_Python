1a5e9934ccc56224bfcfc46cb88e8a766022d328
zc9442664 <1260638417@qq.com>
2020-09-12 10:20:29 +0800

    lab

---
+++ question_bank/637. 二叉树的层平均值.py
@@ -0,0 +1,79 @@
+from queue import Queue
+from queue import deque
+import collections
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def constructTreeNodeDynamic(self, TreeNodeList: list) -> TreeNode:
+        q = Queue(len(TreeNodeList))
+        root = pre = TreeNode(TreeNodeList[0])
+        for i in range(1, len(TreeNodeList)):
+            if TreeNodeList[i] == None:
+                continue
+            cur = TreeNode(TreeNodeList[i])
+            if i % 2 == 1:
+                pre.left = cur
+                q.put(pre.left)
+            else:
+                pre.right = cur
+                q.put(pre.right)
+                if not q.empty():
+                    pre = q.get()
+        return root
+
+    # 广搜
+    def averageOfLevels(self, root: TreeNode) -> list:
+        res = []
+        q = deque([root])
+        while q:
+            nodesum = 0
+            n = len(q)
+            for _ in range(n):
+                node = q.popleft()
+                nodesum += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(nodesum / n)
+        return res
+
+    # 递归深搜
+    def averageOfLevels(self, root: TreeNode) -> list:
+        level_dic = collections.defaultdict(list)
+
+        def dfs(node: TreeNode, depth: int) -> None:
+            if node:
+                level_dic[depth] += [node.val]
+                dfs(node.left, depth + 1)
+                dfs(node.right, depth + 1)
+        dfs(root, 0)
+        return list(sum(vals) / len(vals) for vals in level_dic.values())
+
+    # 迭代深搜
+    def averageOfLevels(self, root: TreeNode) -> list:
+        level_dic = collections.defaultdict(list)
+        stack, node, depth = [], root, -1
+        while stack or node:
+            while node:
+                depth += 1
+                stack.append((depth, node))
+                level_dic[depth] += [node.val]
+                node = node.left
+            depth, node = stack.pop()
+            node = node.right
+        return list(sum(vals) / len(vals) for vals in level_dic.values())
+
+
+nums = [3, 9, 20, None, None, 15, 17]
+test = Solution()
+root = test.constructTreeNodeDynamic(nums)
+print(test.averageOfLevels(root))
