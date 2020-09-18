d0d196982a1802d39289174796f4566ef8c62288
zc9442664 <1260638417@qq.com>
2020-09-18 16:17:58 +0800

    lab

---
+++ question_bank/47. 全排列 II.py
@@ -0,0 +1,28 @@
+class Solution:
+    def permuteUnique(self, nums: list) -> list:
+        def dfs(nums: list, size: int, depth: int, path: list, used: int, res: list):
+            if depth == size:
+                res.append(path.copy())
+                return
+            for i in range(size):
+                if not used[i]:
+                    if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                        continue
+                    used[i] = True
+                    path.append(nums[i])
+                    dfs(nums, size, depth + 1, path, used, res)
+                    used[i] = False
+                    path.pop()
+
+        size = len(nums)
+        if size == 0:
+            return []
+        nums.sort()
+        used = [False] * size
+        res = []
+        dfs(nums, size, 0, [], used, res)
+        return res
+
+
+nums = [1, 1, 2]
+print(Solution().permuteUnique(nums))
--- question_bank/890. 查找和替换模式.py
+++ question_bank/890. 查找和替换模式.py
@@ -3,23 +3,13 @@ class Solution:
         def match(words: str) -> bool:
             m1, m2 = {}, {}
             for w, p in zip(words, pattern):
+                # 存储words -> pattern 的映射
                 if w not in m1:
                     m1[w] = p
+                # 存储pattern -> words 的映射
                 if p not in m2:
                     m2[p] = w
-                if (m1[w], m2[p]) != (p, w):
-                    return False
-            return True
-        list(filter(match, words))
-
-    def findAndReplacePattern(self, words, pattern):
-        def match(word):
-            m1, m2 = {}, {}
-            for w, p in zip(word, pattern):
-                if w not in m1:
-                    m1[w] = p
-                if p not in m2:
-                    m2[p] = w
+                # 当前两个字符与映射不同时 匹配失败
                 if (m1[w], m2[p]) != (p, w):
                     return False
             return True
