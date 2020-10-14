3a80ad2471fc9b37add3766c34d3ffce66d251d3
zc9442664 <1260638417@qq.com>
2020-10-14 09:04:02 +0800

    dell

--- questions/1001-2000/1001-1100/1002. 查找常用字符.py
+++ questions/1001-2000/1001-1100/1002. 查找常用字符.py
@@ -3,16 +3,16 @@ from collections import Counter
 
 class Solution:
     def commonChars(self, A: list) -> list:
-        mincount = [float("inf")] * 26
+        minCount = [float("inf")] * 26
         for word in A:
             count = [0] * 26
             for ch in word:
                 count[ord(ch) - ord('a')] += 1
             for i in range(26):
-                mincount[i] = min(mincount[i], count[i])
+                minCount[i] = min(minCount[i], count[i])
         ans = []
         for i in range(26):
-            ans.extend([chr(i + ord('a'))] * mincount[i])
+            ans.extend([chr(i + ord('a'))] * minCount[i])
         return ans
 
 
