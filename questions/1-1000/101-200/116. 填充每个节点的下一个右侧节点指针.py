d53bdfdc51f65f1b371586399b3934d8b8e8223e
zc9442664 <1260638417@qq.com>
2020-10-15 18:09:14 +0800

    lab

---
+++ questions/1-1000/101-200/116. 填充每个节点的下一个右侧节点指针.py
@@ -0,0 +1,44 @@
+import collections
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    # 层次遍历
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        q = collections.deque([root])
+        while q:
+            size = len(q)
+            for i in range(q):
+                node = q.popleft()
+                if i < size - 1:
+                    node.next = q[0]
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return root
+
+    # 使用已建立的 next 指针
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        leftmost = root
+        while leftmost.left:
+            head = leftmost
+            while head:
+                head.left.next = head.right
+                if head.next:
+                    head.right.next = head.next.left
+                head = head.next
+            leftmost = leftmost.left
+        return root
--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-import complex
-from math import hypot
+metro_areas = [
+    ('Tokyo','JP',36.933,(35.689722,139.691667)),  # ➊
+    ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+    ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+    ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+    ('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833)),
+]
 
-v = 3+4j
-print(v)
-print(abs(v))
+print('{:15} | {:^9} | {:^9}'.format('', 'lat.', 'long.'))
+print('---')
+fmt = '{:15} | {:9.4f} | {:9.4f}'
+for name, cc, pop, (latitude, longitude) in metro_areas:  # ➋
+    if longitude <= 0:  # ➌
+        print(fmt.format(name, latitude, longitude))
