e5a078e502b59e07cc6a6ba5bf5289856f10a955
zcTresure <1260638417@qq.com>
2020-12-14 18:09:00 +0800

    lab

---
+++ questions/1-1000/1-100/49. 字母异位词分组.py
@@ -0,0 +1,29 @@
+# Date:       2020/12/14
+# Coding:      UTF-8
+__author__ = "zcTresure"
+
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = defaultdict(list)
+        for st in strs:
+            key = ''.join(sorted(st))
+            mp[key].append(st)
+        return list(mp.values())
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = defaultdict(list)
+        for st in strs:
+            counts = [0] * 26
+            for ch in st:
+                counts[ord(ch) - ord('a')] += 1
+            mp[tuple(counts)].append(st)
+        return list(mp.values())
+
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+test = Solution()
+print(test.groupAnagrams(strs))
