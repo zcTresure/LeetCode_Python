77f1cc8962b02e0eb83f845dc65803da19b8ca29
zcTresure <1260638417@qq.com>
2020-12-21 13:34:08 +0800

    lab

--- questions/1-1000/301-400/316. 去除重复字母.py
+++ questions/1-1000/301-400/316. 去除重复字母.py
@@ -11,17 +11,20 @@ class Solution:
         for c in s:
             counts[ord(c) - ord('a')] += 1
         for c in s:
+            # 当前字符没有访问过，进行处理判断
             if not visited[ord(c) - 97]:
+                # 当栈不空且栈顶字符小于当前字符判断是否要出栈
                 while strs and strs[-1] > c:
+                    # 当前字符个数不为零时，说明后面还有相同的字符，则弹出栈顶，并重新标记
                     if counts[ord(strs[-1]) - 97] > 0:
                         visited[ord(strs[-1]) - 97] = 0
                         strs.pop()
                     else:
                         break
+                # 标记当前字符已访问
                 visited[ord(c) - 97] = 1
                 strs.append(c)
             counts[ord(c) - 97] -= 1
-            print(strs)
 
         return ''.join(strs)
 
---
+++ questions/1-1000/701-800/746. 使用最小花费爬楼梯.py
@@ -0,0 +1,25 @@
+# Date:       2020/12/21
+# Coding:      UTF-8
+__author__ = "zcTresure"
+
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        for i in range(2, len(cost)):
+            cost[i] = cost[i] + min(cost[i - 1], cost[i - 2])
+        return cost[-1]
+
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        prev = curr = 0
+        for i in range(2, len(cost) + 1):
+            minCost = min(prev + cost[i - 2], curr + cost[i - 1])
+            prev, curr = curr, minCost
+        return curr
+
+
+cost = [0, 0, 0, 0]
+test = Solution()
+print(test.minCostClimbingStairs(cost))
