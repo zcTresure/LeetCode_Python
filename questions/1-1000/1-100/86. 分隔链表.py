345f2c406b09ba11e884e8a401c39b903e6f3ebe
zcTresure <1260638417@qq.com>
2021-01-03 15:36:33 +0800

    lab

---
+++ questions/1-1000/1-100/86. 分隔链表.py
@@ -0,0 +1,57 @@
+# Date:       2021/1/3
+# Coding:      UTF-8
+__author__ = "zcTresure"
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class Solution:
+    # 建立链表
+    def build(self, nums: list) -> ListNode:
+        if not nums:
+            return None
+        head = ListNode(nums[0])
+        pre = head
+        for i in range(1, len(nums)):
+            cur = ListNode(nums[i])
+            pre.next = cur
+            pre = pre.next
+        return head
+
+    # 打印链表
+    def Print(self, head: ListNode) -> None:
+        while head:
+            print(head.val, end='')
+            head = head.next
+            if head:
+                print(' ', end='')
+            else:
+                print()
+
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        h1 = cur1 = ListNode(0)
+        h2 = cur2 = ListNode(0)
+        while head:
+            if head.val < x:
+                cur1.next = head
+                cur1 = cur1.next
+            else:
+                cur2.next = head
+                cur2 = cur2.next
+            head = head.next
+        cur1.next = h2.next
+        cur2.next = None
+        return h1.next
+
+
+nums = [1, 4, 3, 2, 5, 2]
+x = 3
+test = Solution()
+head = test.build(nums)
+head = test.partition(head, 3)
+test.Print(head)
---
+++ questions/1-1000/201-300/239. 滑动窗口最大值.py
@@ -0,0 +1,64 @@
+# Date:       2021/1/2
+# Coding:      UTF-8
+__author__ = "zcTresure"
+
+from typing import List
+from collections import deque
+import heapq
+
+
+class Solution:
+    # 优先队列
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        q = [(-nums[i], i) for i in range(k)]
+        heapq.heapify(q)
+        ans = [-q[0][0]]
+        for i in range(k, n):
+            heapq.heappush(q, (-nums[i], i))
+            while q[0][1] <= i - k:
+                heapq.heappop(q)
+            ans.append(-q[0][0])
+        return ans
+
+    # 单调队列
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        q = deque()
+        for i in range(k):
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            q.append(i)
+        ans = [nums[q[0]]]
+        for i in range(k, n):
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            q.append(i)
+            while q[0] <= i - k:
+                q.popleft()
+            ans.append(nums[q[0]])
+        return ans
+
+    # 分块 + 预处理
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        prefixMax, suffixMax = [0] * n, [0] * n
+        for i in range(n):
+            if i % k == 0:
+                prefixMax[i] = nums[i]
+            else:
+                prefixMax[i] = max(prefixMax[i - 1], nums[i])
+        for i in range(n - 1, -1, -1):
+            if i == n - 1 or (i + 1) % k == 0:
+                suffixMax[i] = nums[i]
+            else:
+                suffixMax[i] = max(suffixMax[i + 1], nums[i])
+
+        ans = [max(suffixMax[i], prefixMax[i + k - 1]) for i in range(n - k + 1)]
+        return ans
+
+
+test = Solution()
+nums = [1, -1]
+k = 1
+print(test.maxSlidingWindow(nums, k))
