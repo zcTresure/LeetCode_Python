8451ee8b4c446c9d8e458d112e954a0852ab4cec
zcTresure <1260638417@qq.com>
2021-03-10 21:20:23 +0800

    lab

---
+++ questions/1-1000/201-300/224. 基本计算器.py
@@ -0,0 +1,33 @@
+# Date:       2021/3/10
+# Coding:      UTF-8
+__author__ = "zcTresure"
+
+
+class Solution(object):
+    def calculate(self, s: str) -> int:
+        res, num, sign = 0, 0, 1
+        stack = []
+        for c in s:
+            if c.isdigit():
+                num = 10 * num + int(c)
+            elif c == "+" or c == "-":
+                res += sign * num
+                num = 0
+                sign = 1 if c == "+" else -1
+            elif c == "(":
+                stack.append(res)
+                stack.append(sign)
+                res = 0
+                sign = 1
+            elif c == ")":
+                res += sign * num
+                num = 0
+                res *= stack.pop()
+                res += stack.pop()
+        res += sign * num
+        return res
+
+
+s = "1+1"
+test = Solution()
+print(test.calculate(s))
