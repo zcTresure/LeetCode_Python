28ddb277455935b24b7be88aa9699f587c265a1d
zcTresure <1260638417@qq.com>
2021-03-24 22:12:41 +0800

    lab

---
+++ questions/1-1000/801-900/872. 叶子相似的树.py
@@ -0,0 +1,64 @@
+# # File Name:  872. 叶子相似的树
+# # date:       2021/3/24
+# # encode:      UTF-8
+# __author__ = 'zcTresure'
+#
+# import collections
+#
+#
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#
+#
+# class Solution:
+#     # 二叉树的建立
+#     def buildBinaryTree(self, nums: list) -> TreeNode:
+#         if nums[0] == None:
+#             return None
+#         root = TreeNode(nums[0])
+#         Nodes, index = [root], 1
+#         for node in Nodes:
+#             if node != None:
+#                 node.left = TreeNode(nums[index]) if nums[index] != None else None
+#                 Nodes.append(node.left)
+#                 index += 1
+#                 if index == len(nums):
+#                     return root
+#                 node.right = TreeNode(nums[index]) if nums[index] != None else None
+#                 Nodes.append(node.right)
+#                 index += 1
+#                 if index == len(nums):
+#                     return root
+#
+#     def leafSimilar(self, root1: TreeNode, root2: TreeNode) -> bool:
+#         def preOrder(root: TreeNode):
+#             if root:
+#                 if not root.left and not root.right:
+#                     yield root.val
+#                 yield from preOrder(root.left)
+#                 yield from preOrder(root.right)
+#
+#         return list(preOrder(root1)) == list(preOrder(root2))
+#
+#
+# nums1 = [1]
+# nums2 = [2]
+# test = Solution()
+# root1 = test.buildBinaryTree(nums1)
+# root2 = test.buildBinaryTree(nums2)
+# print(test.leafSimilar(root1, root2))
+
+
+def foo():
+    print("starting...")
+    while True:
+        res = yield 4
+        print("res:",res)
+g = foo()
+print(next(g))
+print("*"*20)
+print(next(g))
---
+++ questions/面试题/面试题 04.04. 检查平衡性.py
@@ -0,0 +1,68 @@
+# File Name:  面试题 04.04. 检查平衡性
+# date:       2021/3/24
+# encode:      UTF-8
+__author__ = 'zcTresure'
+
+import collections
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    # 二叉树的建立
+    def buildBinaryTree(self, nums: list) -> TreeNode:
+        if nums[0] == None:
+            return None
+        root = TreeNode(nums[0])
+        Nodes, index = [root], 1
+        for node in Nodes:
+            if node != None:
+                node.left = TreeNode(nums[index]) if nums[index] != None else None
+                Nodes.append(node.left)
+                index += 1
+                if index == len(nums):
+                    return root
+                node.right = TreeNode(nums[index]) if nums[index] != None else None
+                Nodes.append(node.right)
+                index += 1
+                if index == len(nums):
+                    return root
+
+    # 自顶向下的递归
+    def isBalanced(self, root: TreeNode) -> bool:
+        def height(root: TreeNode) -> int:
+            if not root:
+                return 0
+            return max(height(root.left), height(root.right)) + 1
+
+        if not root:
+            return True
+        return abs(height(root.left) - height(root.right)) <= 1 and self.isBalanced(root.left) and self.isBalanced(
+            root.right)
+
+    # 自底向上的递归
+    def isBalanced(self, root: TreeNode) -> bool:
+        def height(root: TreeNode) -> int:
+            if not root:
+                return 0
+            left_height = height(root.left)
+            right_height = height(root.right)
+            if left_height == -1 or right_height == -1 or abs(left_height - right_height) > 1:
+                return -1
+            else:
+                return max(left_height, right_height) + 1
+
+        return height(root) >= 0
+
+
+# nums = [3, 9, 20, None, None, 15, 7]
+nums = [1, 2, 2, 3, 3, None, None, 4, 4]
+test = Solution()
+root = test.buildBinaryTree(nums)
+print(test.isBalanced(root))
