ee0e9fb17e182e2fbdec3732575a523697da6f59
zcTresure <1260638417@qq.com>
2021-03-31 23:30:59 +0800

    dell

--- questions/1001-2000/1401-1500/1457. 二叉树中的伪回文路径.py
+++ questions/1001-2000/1401-1500/1457. 二叉树中的伪回文路径.py
@@ -41,7 +41,7 @@ class Solution:
         self.preOrder(root.right)
 
     def pseudoPalindromicPaths(self, root: TreeNode) -> int:
-        self.count = 0
+        self.count = 0  # 记录伪回文数
 
         def dfs(node, record=0):  # record 位运算初始记录为零
             if node:
@@ -50,7 +50,7 @@ class Solution:
                 dfs(node.right, record)  # 递归调用归右子树
                 if not node.left and not node.right:  # 为叶子节点判断是否为伪回文
                     if bin(record).count('1') < 2:  # 只有 record 中 1 出现的次数少于 2 才是伪回文 回文中心最多只有一个
-                        self.count += 1
+                        self.count += 1  # 伪回文数加 1
 
         dfs(root)
         return self.count
