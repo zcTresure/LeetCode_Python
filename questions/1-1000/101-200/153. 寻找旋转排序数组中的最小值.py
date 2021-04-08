08ce2ddc465a6876a452efdefa68b9dbb0f4d9d0
zcTresure <1260638417@qq.com>
2021-04-08 10:26:03 +0800

    lab

--- questions/1-1000/101-200/153. 寻找旋转排序数组中的最小值.py
+++ questions/1-1000/101-200/153. 寻找旋转排序数组中的最小值.py
@@ -1,16 +1,16 @@
 class Solution:
     # 二分查找
     def findMin(self, nums: list) -> int:
-        l, h = 0, len(nums) - 1
-        while l < h:
-            m = (h + l) >> 1
-            if nums[m] < nums[h]:
-                h = m
-            elif nums[m] > nums[h]:
-                l = m + 1
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (right - left) // 2 + left
+            if nums[mid] < nums[right]:
+                right = mid
+            elif nums[mid] > nums[right]:
+                left = mid + 1
             else:
-                h -= 1
-        return nums[l]
+                right -= 1
+        return nums[left]
 
 
 nums = [3, 4, 5, 1, 2]
