d723e791f454169fc4815706c4758efd1df7720a
zcTresure <1260638417@qq.com>
2021-04-09 08:35:04 +0800

    lab

--- questions/1-1000/101-200/154. 寻找旋转排序数组中的最小值 II.py
+++ questions/1-1000/101-200/154. 寻找旋转排序数组中的最小值 II.py
@@ -1,15 +1,23 @@
+# File Name:  154. 寻找旋转排序数组中的最小值 II
+# date:       2021/4/9
+# encode:      UTF-8
+__author__ = 'zcTresure'
+
+from typing import List
+
+
 class Solution:
-    def findMin(self, nums: list) -> int:
-        l, h = 0, len(nums) - 1
-        while l < h:
-            m = (h + l) >> 1
-            if nums[m] < nums[h]:
-                h = m
-            elif nums[m] > nums[h]:
-                l = m + 1
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] < nums[right]:
+                right = mid
+            elif nums[mid] > nums[right]:
+                left = mid + 1
             else:
-                h -= 1
-        return nums[l]
+                right = right - 1
+        return nums[left]
 
 
 nums = [1, 3, 5]
