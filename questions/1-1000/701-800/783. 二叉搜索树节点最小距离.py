4a7e5baf829ac7bbb72aaa38a0f7a0bd0ade6a9e
zcTresure <1260638417@qq.com>
2021-04-13 08:06:46 +0800

    lab

--- questions/1-1000/701-800/783. 二叉搜索树节点最小距离.py
+++ questions/1-1000/701-800/783. 二叉搜索树节点最小距离.py
@@ -1,22 +1,27 @@
-import collections
+# File Name:  783. 二叉搜索树节点最小距离
+# date:       2021/4/13
+# encode:      UTF-8
+__author__ = 'zcTresure'
 
 
 # Definition for a binary tree node.
 class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
     def constructBTree(self, nums: list) -> TreeNode:
         if nums[0] == None:
-            return None
+            return TreeNode()
         root = TreeNode(nums[0])
         Nodes, index = [root], 1
         for node in Nodes:
             if node != None:
+                if index == len(nums):
+                    return root
                 node.left = TreeNode(
                     nums[index]) if nums[index] != None else None
                 Nodes.append(node.left)
@@ -27,25 +32,8 @@ class Solution:
                     nums[index]) if nums[index] != None else None
                 Nodes.append(node.right)
                 index += 1
-                if index == len(nums):
-                    return root
 
-    def getMinimumDifference(self, root: TreeNode) -> int:
-        def dfs(node: TreeNode):
-            if not node:
-                return
-            dfs(node.left)
-            nums.append(node.val)
-            dfs(node.right)
-
-        nums = []
-        dfs(root)
-        ans = float("inf")
-        for i in range(1, len(nums)):
-            ans = min(ans, nums[i] - nums[i - 1])
-        return ans
-
-    def getMinimumDifference(self, root: TreeNode) -> int:
+    def minDiffInBST(self, root: TreeNode) -> int:
         def dfs(node: TreeNode):
             nonlocal pre, ans
             if not node:
@@ -64,4 +52,4 @@ class Solution:
 nums = [236, 104, 701, None, 227, None, 911]
 test = Solution()
 root = test.constructBTree(nums)
-print(test.getMinimumDifference(root))
+print(test.minDiffInBST(root))
