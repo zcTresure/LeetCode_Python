172eabf4ef9d2bd25af5fd6d9d27969dbc7222fe
zcTresure <1260638417@qq.com>
2021-05-13 10:10:16 +0800

    lab

--- questions/1000-1999/1200-1299/1269. 停在原地的方案数.py
+++ questions/1000-1999/1200-1299/1269. 停在原地的方案数.py
@@ -10,7 +10,7 @@ class Solution:
         mod = 10 ** 9 + 7  # 最大数求余
         maxColumn = min(arrLen - 1, steps)  # 距离原点最远距离
         dp = [[0] * (maxColumn + 1) for _ in range(steps + 1)]
-        dp[0][0] = 1
+        dp[0][0] = 1  # 初始化原点，站在原地不动
         for i in range(1, steps + 1):
             for j in range(maxColumn + 1):
                 dp[i][j] = dp[i - 1][j]  # 站在原地不动
