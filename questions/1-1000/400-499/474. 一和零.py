419b82053daf43d46e26e3a1b9859dd7af4dfaea
zcTresure <1260638417@qq.com>
2021-06-06 10:12:23 +0800

    lab

--- questions/1-1000/400-499/474. 一和零.py
+++ questions/1-1000/400-499/474. 一和零.py
@@ -1,20 +1,24 @@
+# -*- coding: utf-8 -*-
+# File:     474. 一和零.py
+# Date:     2021/6/6
+# Software: PyCharm
+__author__ = 'zcFang'
+
+
 class Solution:
     def findMaxForm(self, strs: list, m: int, n: int) -> int:
-        if len(strs) == 0:
-            return 0
-        else:
-            res = [[0] * (n + 1) for _ in range(m + 1)]
-            for string in strs:
-                ones = string.count("1")
-                zeroes = string.count("0")
-                for i in range(m, zeroes - 1, -1):
-                    for j in range(n, ones - 1, -1):
-                        res[i][j] = max(
-                            res[i][j], res[i - zeroes][j - ones] + 1)
-            return res[m][n]
+        if len(strs) == 0: return 0
+        res = [[0] * (n + 1) for _ in range(m + 1)]
+        for string in strs:
+            ones = string.count("1")
+            zeroes = string.count("0")
+            for i in range(m, zeroes - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    res[i][j] = max(res[i][j], res[i - zeroes][j - ones] + 1)
+        return res[m][n]
 
 
-Array = {"10", "0001", "111001", "1", "0"}
+Array = ["10", "0001", "111001", "1", "0"]
 m, n = 5, 3
 test = Solution()
 print(test.findMaxForm(Array, m, n))
