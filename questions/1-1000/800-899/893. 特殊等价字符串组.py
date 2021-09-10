9abe29ea26be5e05ec0b443352cece281a01c1f7
zcTresure <1260638417@qq.com>
2021-09-10 22:34:17 +0800

    lab

--- questions/1-1000/800-899/893. 特殊等价字符串组.py
+++ questions/1-1000/800-899/893. 特殊等价字符串组.py
@@ -11,7 +11,7 @@ class Solution:
     def numSpecialEquivGroups(self, words: List[str]) -> int:
         ans = set()
         for word in words:
-            count = [0] * 52
+            count = [0] * 52 # 两倍空间, 存储奇下标[0,26), 偶下标[26,52)
             for i, c in enumerate(word):
                 count[ord(c) - ord('a') + 26 * (i % 2)] += 1
             ans.add(tuple(count))
