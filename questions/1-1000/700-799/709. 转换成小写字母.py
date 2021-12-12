80499cb80a43b1173ead4ecc98b13412b6decbd5
zcTresure <1260638417@qq.com>
2021-12-12 12:07:42 +0800

    lab

--- questions/1-1000/700-799/709. 转换成小写字母.py
+++ questions/1-1000/700-799/709. 转换成小写字母.py
@@ -7,6 +7,9 @@ __author__ = 'zcFang'
 
 class Solution:
     def toLowerCase(self, s: str) -> str:
+        return "".join(chr(asc | 32) if 65 <= (asc := ord(ch)) <= 90 else ch for ch in s)
+
+    def toLowerCase(self, s: str) -> str:
         return s.lower()
 
 
