baff6a2ad1504084d73a2f7967742e72fef3628c
zcTresure <1260638417@qq.com>
2022-03-13 09:58:40 +0800

    Default Changelist

--- questions/2000-2999/2049. 统计最高分的节点数目.py
+++ questions/2000-2999/2049. 统计最高分的节点数目.py
@@ -11,9 +11,10 @@ class Solution:
     def countHighestScoreNodes(self, parents: List[int]) -> int:
         n = len(parents)
         children = [[] for _ in range(n)]
-        for node, p in enumerate(parents):
-            if p != -1:
-                children[p].append(node)
+        for node, parent in enumerate(parents):
+            if parent != -1:
+                children[parent].append(node)
+
         max_score, cnt = 0, 0
 
         def dfs(node: int) -> int:
@@ -22,12 +23,14 @@ class Solution:
             for ch in children[node]:
                 sz = dfs(ch)
                 score *= sz
-                size *= size
+                size -= sz
+            if node != 0:
+                score *= size
             nonlocal max_score, cnt
             if score == max_score:
                 cnt += 1
             elif score > max_score:
-                max_score = score
+                max_score, cnt = score, 1
             return n - size
 
         dfs(0)
